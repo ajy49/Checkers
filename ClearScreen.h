ecbc21135826e6aefb572c47e084d1383ff0b0d8
ajy49 <44096653+ajy49@users.noreply.github.com>
2018-10-23 16:03:30 -0400

    Create ClearScreen.h

---
+++ ClearScreen.h
@@ -0,0 +1,46 @@
+#ifndef CLEARSCREEN_H_INCLUDED
+#define AI_H_INCLUDED
+
+/*The function here was provided by a Microsoft Windows Dev Center support page as an alternative to using system() to clear to screen
+(https://msdn.microsoft.com/en-us/library/windows/desktop/ms682022(v=vs.85).aspx)*/
+
+#include <Windows.h>
+
+void ClearScreen()
+{
+	HANDLE hStdOut;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD count;
+	DWORD cellCount;
+	COORD homeCoords = { 0, 0 };
+
+	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+	/* Get the number of cells in the current buffer */
+	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
+	cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+	/* Fill the entire buffer with spaces */
+	if (!FillConsoleOutputCharacter(
+		hStdOut,
+		(TCHAR) ' ',
+		cellCount,
+		homeCoords,
+		&count
+	)) return;
+
+	/* Fill the entire buffer with the current colors and attributes */
+	if (!FillConsoleOutputAttribute(
+		hStdOut,
+		csbi.wAttributes,
+		cellCount,
+		homeCoords,
+		&count
+	)) return;
+
+	/* Move the cursor home */
+	SetConsoleCursorPosition(hStdOut, homeCoords);
+}
+
+#endif
